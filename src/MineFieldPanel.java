43992beaeb4617f71e4d1dd14a12f573da44ce33
Travis Schimmel <Travis Schimmel@LAPTOP-HS14RU24>
2020-04-13 09:13:15 -0600

    The skeleton for MineFieldPanel

--- src/MineFieldPanel.java
+++ src/MineFieldPanel.java
@@ -1,5 +1,56 @@
+import java.awt.event.ActionListener;
+
 import javax.swing.JPanel;
 
 public class MineFieldPanel extends JPanel {
 
+	private final int DEFAULT_GRID_SIZE = 10;
+	private final double MIN_MINE_PERCENT = 0;
+	private final double DEFAULT_MINE_PERCENT = 0;
+	private final double MAX_MINE_PERCENT = 0;
+	private MineFieldButton[][] grid;
+	
+	public void MineFieldPanel(ActionListener listener) {
+		
+	}
+	
+	public void MineFieldPanel(int something, ActionListener listener) {
+		
+	}
+	
+	public int getGridSize() {
+		return DEFAULT_GRID_SIZE;
+	}
+	
+	public void hideMines() {
+		
+	}
+	
+	public void showMines() {
+		
+	}
+	
+	public void hidePath() {
+		
+	}
+	
+	public void showPath() {
+		
+	}
+	
+	public void deactivate() {
+		
+	}
+	
+	private void setMines(double something) {
+		
+	}
+	
+	private void addAllNeighbors() {
+		
+	}
+	
+	private void addNeighbor(int a, int b, int c, int d) {
+		
+	}
 }
