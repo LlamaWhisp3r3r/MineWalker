7ca55db022e3ea15d4b56a0c8d5bbea54d924b74
Kokob <Kokob@LAPTOP-OGU48C51>
2020-04-13 08:37:34 -0600

    Here's the skeleton code. pretty scary.

--- src/MineFieldButton.java
+++ src/MineFieldButton.java
@@ -1,5 +1,102 @@
+import java.awt.Color;
+
 import javax.swing.JButton;
 
 public class MineFieldButton extends JButton {
-
+	
+	public MineFieldButton(ActionListener) {
+		
+	}
+	
+	public void visit() {
+		
+	}
+	
+	public void addNeighbor(MineFieldButton button) {
+		
+	}
+	
+	public void setIsStart(boolean newStart) {
+		
+	}
+	
+	public boolean isStart() {
+		
+	}
+	
+	public void setIsEnd(boolean newEnd) {
+		
+	}
+	
+	public boolean isEnd() {
+		
+	}
+	
+	public void setIsMine(boolean newMine) {
+		
+	}
+	
+	public boolean isMine() {
+		
+	}
+	
+	public void setOnPath(boolean newPath) {
+		
+	}
+	
+	public boolean isOnPath() {
+		
+	}
+	
+	public void showMine() {
+		
+	}
+	
+	public void hideMine() {
+		
+	}
+	
+	public void showPath() {
+		
+	}
+	
+	public void hidePath() {
+		
+	}
+	
+	public void activate() {
+		
+	}
+	
+	public void deactivate() {
+		
+	}
+	
+	
+	// I don't know if these were supposed to be private.
+	public void setCurrentLocationText(boolean current) {
+		
+	}
+	
+	public int getNearbyMineCount() {
+		
+	}
+	
+	public void activateNeighbors() {
+		
+	}
+	
+	public void deactivateNeighbors() {
+		
+	}
+	
+	public void deactivateNeighborsOfNeighbors() {
+		
+	}
+	
+	public Color getNearbyMineColor(int mineCount) {
+		
+	}
+	
+	
 }
