c8999c9ae85001f3ea848a506d445b00f73ec5a6
Travis Schimmel <Travis Schimmel@LAPTOP-HS14RU24.lan>
2020-04-15 09:12:12 -0600

    Added the GUI to MineFieldPanel

--- src/MineFieldPanel.java
+++ src/MineFieldPanel.java
@@ -1,3 +1,5 @@
+import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
@@ -8,38 +10,105 @@ public class MineFieldPanel extends JPanel {
 	private final double MIN_MINE_PERCENT = 0;
 	private final double DEFAULT_MINE_PERCENT = 0;
 	private final double MAX_MINE_PERCENT = 0;
+	private int gridSize;
 	private MineFieldButton[][] grid;
 	
-	public void MineFieldPanel(ActionListener listener) {
+	public MineFieldPanel(ActionListener listener) {
 		
+		grid = new MineFieldButton[DEFAULT_GRID_SIZE][DEFAULT_GRID_SIZE];
+		
+		//Sets the size of the grid and the number of buttons in a row.
+		this.setLayout(new GridLayout(grid.length, grid[0].length, 1, 1));
+		this.setPreferredSize(new Dimension(1, 1));
+		
+		// Creates each button, adds an action listener to them, and adds them to the panel
+		for (int i = 0; i < grid.length; i++) {
+				
+			for (int j = 0; j < grid[0].length; j++) {
+						
+				grid[i][j] = new MineFieldButton(listener);
+				this.add(grid[i][j]);
+			}	
+		}
 	}
 	
-	public void MineFieldPanel(int something, ActionListener listener) {
+	public MineFieldPanel(int gridSize, ActionListener listener) {
 		
+		this.gridSize = gridSize;
+		grid = new MineFieldButton[this.gridSize][this.gridSize];
+		
+		//Sets the size of the grid and the number of buttons in a row.
+		this.setLayout(new GridLayout(grid.length, grid[0].length, 1, 1));
+		this.setPreferredSize(new Dimension(1, 1));
+		
+		// Creates each button, adds an action listener to them, and adds them to the panel
+		for (int i = 0; i < grid.length; i++) {
+				
+			for (int j = 0; j < grid[0].length; j++) {
+						
+				grid[i][j] = new MineFieldButton(listener);
+				this.add(grid[i][j]);
+			}		
+		}
 	}
 	
 	public int getGridSize() {
-		return DEFAULT_GRID_SIZE;
+		return gridSize;
 	}
 	
 	public void hideMines() {
-		
+		for (int i = 0; i < grid.length; i++) {
+			
+			for (int j = 0; j < grid[0].length; j++) {
+						
+				grid[i][j].hideMine();
+				this.add(grid[i][j]);
+			}		
+		}
 	}
 	
 	public void showMines() {
-		
+		for (int i = 0; i < grid.length; i++) {
+			
+			for (int j = 0; j < grid[0].length; j++) {
+						
+				grid[i][j].showMine();
+				this.add(grid[i][j]);
+			}		
+		}
 	}
 	
 	public void hidePath() {
-		
+		for (int i = 0; i < grid.length; i++) {
+			
+			for (int j = 0; j < grid[0].length; j++) {
+						
+				grid[i][j].hidePath();
+				this.add(grid[i][j]);
+			}		
+		}
 	}
 	
 	public void showPath() {
-		
+		for (int i = 0; i < grid.length; i++) {
+			
+			for (int j = 0; j < grid[0].length; j++) {
+						
+				grid[i][j].showPath();
+				this.add(grid[i][j]);
+			}		
+		}
 	}
 	
 	public void deactivate() {
-		
+		for (int i = 0; i < grid.length; i++) {
+			
+			for (int j = 0; j < grid[0].length; j++) {
+						
+				grid[i][j].deactivate();
+				this.add(grid[i][j]);
+			}		
+		}
 	}
 	
 	private void setMines(double something) {
@@ -54,3 +123,5 @@ public class MineFieldPanel extends JPanel {
 		
 	}
 }
+
+
